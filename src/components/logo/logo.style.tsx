7812881c52aca774fff4e82a3ef683dc06dd7978
Nazareno <Aeliner123@gmail.com>
2020-09-29 09:12:53 -0300

    Initial commit

---
+++ src/components/logo/logo.style.tsx
@@ -0,0 +1,18 @@
+const styles = {
+	wrapper: {
+		display: 'inline-flex',
+		alignItems: 'center',
+		a: {
+			display: 'flex',
+			transition: 'all 0.2s ease',
+			':hover': {
+				opacity: 0.89,
+			},
+		},
+	},
+	logo: {
+		width: [84, 250],
+	},
+};
+
+export default styles;
